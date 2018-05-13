c73f22856c7442efdea630fe3ad643e55d9019e7
zondahuman <zondahuman@gmail.com>
2018-05-13 14:59:10 +0800

    add

--- serialize-kryo/src/main/java/com/abin/lee/serialize/kryo/KryoSerizlize.java
+++ serialize-kryo/src/main/java/com/abin/lee/serialize/kryo/KryoSerizlize.java
@@ -3,6 +3,9 @@ package com.abin.lee.serialize.kryo;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
+import com.esotericsoftware.kryo.serializers.BeanSerializer;
+import com.esotericsoftware.kryo.serializers.JavaSerializer;
+import de.javakaffee.kryoserializers.SynchronizedCollectionsSerializer;
 
 import java.util.Arrays;
 
@@ -11,14 +14,47 @@ import java.util.Arrays;
  * serialize-svr
  * com.abin.lee.serialize.kryo
  * https://blog.csdn.net/derrantcm/article/details/72861680
+ * https://www.cnblogs.com/holoyong/p/7420880.html
+ * https://blog.csdn.net/rocklee/article/details/26451739
  */
 public class KryoSerizlize {
     private static Kryo kryo = new Kryo();
 
-    public static byte[] serialize(Object object)  {
+    static {
+        SynchronizedCollectionsSerializer.registerSerializers(kryo);
+        kryo.addDefaultSerializer(java.lang.Throwable.class, new JavaSerializer());
+    }
 
+    /**
+     * 第一组采用writeObject, readObject
+     * 1，kryo.writeObject，这种方法只会序列化对象实例，而不会记录对象所属类的任何信息。优势是最节省空间，劣势是在反序列化时，需要提供类作为模板才能顺利反序列。反序列化时使用readObject。
+     * @param object
+     * @return
+     */
+    public static byte[] serializeObject(Object object)  {
         System.out.println("[serialize]" + object);
+        byte[] buffer = new byte[2048];
+        Output output = new Output(buffer);
+        kryo.writeObject(output, object);
+        return output.toBytes();
+    }
 
+    public static <T> T  deserializeObject(byte[] bytes, Class<T> clazz)  {
+        System.out.println("[deserialize]" + Arrays.toString(bytes));
+        Input input = new Input(bytes);
+        @SuppressWarnings("unchecked")
+        T t = (T) kryo.readObject(input, clazz);
+        return t;
+    }
+
+
+    /**
+     * 2，直接kryo.writeClassAndObject，这种方法会先将对象所属类的全限定名序列化，然后再依次序列化对象实例的成员。优势是完全动态序列化，整个kryo周期都不需要提供类信息。反序列化时使用readClassAndObject
+     * @param object
+     * @return
+     */
+    public static byte[] serialize(Object object)  {
+        System.out.println("[serialize]" + object);
         byte[] buffer = new byte[2048];
         Output output = new Output(buffer);
         kryo.writeClassAndObject(output, object);
@@ -26,14 +62,43 @@ public class KryoSerizlize {
     }
 
     public static Object deserialize(byte[] bytes)  {
-
         System.out.println("[deserialize]" + Arrays.toString(bytes));
-
         Input input = new Input(bytes);
         @SuppressWarnings("unchecked")
         Object t = (Object) kryo.readClassAndObject(input);
         return t;
     }
+
+
+    /**
+     * 3，先注册，再kryo.writeClassAndObject，这种方式时最理想的，其结合了前两种优势，又有效规避了劣势，事先将需要序列化的类注册给kryo（此时类和唯一id绑定），
+     * 之后使用writeClassAndObject序列化时，只会序列化注册id，而不会序列化类的全限定名了，这样大大节省了空间（通常只比writeObject多一个字节）。
+     * 反序列化时使用readClassAndObject。注意序列化和反序列的kryo的注册信息应当保持一致。
+     * @param object
+     * @return
+     */
+    public static byte[] serializeRegistry(Object object, Class<?> clazz)  {
+        System.out.println("[serialize]" + object);
+        kryo.register(clazz.getClass(),new BeanSerializer(kryo, clazz.getClass()));
+        byte[] buffer = new byte[2048];
+        Output output = new Output(buffer);
+        kryo.writeObject(output, object);
+        return output.toBytes();
+    }
+
+    public static Object deserializeRegistry(byte[] bytes, Class<?> clazz)  {
+        System.out.println("[deserialize]" + Arrays.toString(bytes));
+//        kryo.setRegistrationRequired(true);
+        kryo.register(clazz.getClass(),new BeanSerializer(kryo, clazz.getClass()));
+//        kryo.register(clazz.getClass());
+//        SynchronizedCollectionsSerializer.registerSerializers(kryo);
+        Input input = new Input(bytes);
+        @SuppressWarnings("unchecked")
+        Object t = (Object)kryo.readObject(input, clazz.getClass());
+        return t;
+    }
+
+
 
 
 }
--- serialize-kryo/src/test/java/com/abin/lee/serialize/kryo/test/KryoSerizlizeTest.java
+++ serialize-kryo/src/test/java/com/abin/lee/serialize/kryo/test/KryoSerizlizeTest.java
@@ -1,12 +1,17 @@
 package com.abin.lee.serialize.kryo.test;
 
+import avro.shaded.com.google.common.collect.Maps;
 import com.abin.lee.serialize.kryo.KryoSerizlize;
 import com.abin.lee.serialize.kryo.common.util.JsonUtil;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.junit.Test;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by abin on 2018/4/15 15:21.
@@ -29,7 +34,6 @@ public class KryoSerizlizeTest {
         System.out.println("result1=" + result1);
         User userResult = (User)KryoSerizlize.deserialize(result1);
         System.out.println("userResult=" + JsonUtil.toJson(userResult));
-
     }
 
     @Test
@@ -41,16 +45,85 @@ public class KryoSerizlizeTest {
         System.out.println("byteResult=" + byteResult);
         Integer objectResult = (Integer)KryoSerizlize.deserialize(byteResult);
         System.out.println("objectResult=" + JsonUtil.toJson(objectResult));
+    }
 
+    @Test
+    public void testKryo3(){
+        Map<String, Object> map = Maps.newHashMap();
+        map.put("id", 1);
+        map.put("name", "lee");
+        map.put("createTime", new Date());
+        System.out.println("map=" + JsonUtil.toJson(map));
+        byte[] result1 = KryoSerizlize.serialize(map);
+        System.out.println("result1=" + result1);
+        Map<String, Object> userResult = (Map<String, Object>)KryoSerizlize.deserialize(result1);
+        System.out.println("userResult=" + JsonUtil.toJson(userResult));
+    }
+
+    @Test
+    public void testKryo4(){
+        Map<String, Object> map = Maps.newHashMap();
+        User user = new User();
+        user.setCreateTime(new Date());
+        user.setId((int)(Math.random()*100));
+        user.setName("lee");
+        user.setPrice(new BigDecimal("" + (int)(Math.random()*10000)));
+        map.put("user", user);
+
+        System.out.println("map=" + JsonUtil.toJson(map));
+        byte[] result1 = KryoSerizlize.serialize(map);
+        System.out.println("result1=" + result1);
+        Map<String, Object> userResult = (Map<String, Object>)KryoSerizlize.deserialize(result1);
+        System.out.println("userResult=" + JsonUtil.toJson(userResult));
+    }
+
+
+    @Test
+    public void testKryo5(){
+        Map<String, Object> map = Maps.newHashMap();
+        User user = new User();
+        user.setCreateTime(new Date());
+        user.setId((int)(Math.random()*100));
+        user.setName("lee");
+        user.setPrice(new BigDecimal("" + (int)(Math.random()*10000)));
+        map.put("user", user);
+
+        System.out.println("map=" + JsonUtil.toJson(map));
+        byte[] result1 = KryoSerizlize.serializeObject(map);
+        System.out.println("result1=" + result1);
+        Map<String, Object> userResult = (Map<String, Object>)KryoSerizlize.deserializeObject(result1, map.getClass());
+        System.out.println("userResult=" + JsonUtil.toJson(userResult));
+    }
+
+
+    @Test
+    public void testKryo6(){
+        Map<String, Object> map = Maps.newHashMap();
+        User user = new User();
+        user.setCreateTime(new Date());
+        user.setId((int)(Math.random()*100));
+        user.setName("lee");
+        user.setPrice(new BigDecimal("" + (int)(Math.random()*10000)));
+        map.put("user", user);
+
+        System.out.println("map=" + JsonUtil.toJson(map));
+        byte[] result1 = KryoSerizlize.serializeRegistry(map, map.getClass());
+        System.out.println("result1=" + result1);
+//        Map<String, Object> userResult = (Map<String, Object>)KryoSerizlize.deserializeRegistry(result1, map.getClass());
+        Object userResult = (Object)KryoSerizlize.deserializeRegistry(result1, map.getClass());
+        System.out.println("userResult=" + JsonUtil.toJson(userResult));
     }
 
 
     @Data
-    public static class User{
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class User implements Serializable {
         private Integer id;
         private String name;
         private BigDecimal price;
         private Date createTime;
+
     }
 
 
